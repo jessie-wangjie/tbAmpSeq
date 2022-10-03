93b9d1b68aa7556ec9aea585b0139fcca11896da
jwang <jwang@tome.bio>
2022-10-03 12:54:59 -0400

    fix primer query

--- tbOnT.B2B.py
+++ tbOnT.B2B.py
@@ -56,13 +56,33 @@ def main():
 
         # Get primer information
         if pp_id:
+            # cur.execute(
+            #    "select p1.chromosome, p1.start, p2.end, p1.genome_build, target_gene.direction_of_transcription from primer_pair "
+            #   "join primer as p1 on p1.id = primer_pair.forward_primer "
+            #    "join primer as p2 on p2.id = primer_pair.reverse_primer "
+            #    "join target_gene on target_gene.id = p1.gene_or_target_name "
+            #    "where primer_pair.file_registry_id$ = %s", [pp_id])
+            # target_chr, wt_start, wt_end, genome_build, target_strand = cur.fetchone()
+
             cur.execute(
-                "select p1.chromosome, p1.start, p2.end, p1.genome_build, target_gene.direction_of_transcription from primer_pair "
+                "select p1.file_registry_id$, p2.file_registry_id$ from primer_pair "
                 "join primer as p1 on p1.id = primer_pair.forward_primer "
                 "join primer as p2 on p2.id = primer_pair.reverse_primer "
-                "join target_gene on target_gene.id = p1.gene_or_target_name "
-                "where primer_pair.file_registry_id$ = %s", [pp_id])
-            target_chr, wt_start, wt_end, genome_build, target_strand = cur.fetchone()
+                "where primer_pair.file_registry_id$ = %s", [sample["PP ID"]])
+            fp_id, rp_id = cur.fetchone()
+
+            cur.execute(
+                "select dna_oligo.bases, target_gene.chromosome, target_gene.genome_build, target_gene.direction_of_transcription from primer "
+                "join target_gene on target_gene.id = primer.gene_or_target_name "
+                "join dna_oligo on dna_oligo.id = primer.id "
+                "where primer.file_registry_id$ = %s", [fp_id])
+            fp_seq, target_chr, genome_build, target_strand = cur.fetchone()
+
+            cur.execute(
+                "select dna_oligo.bases from primer "
+                "join dna_oligo on dna_oligo.id = primer.id "
+                "where primer.file_registry_id$ = %s", [rp_id])
+            rp_seq = cur.fetchone()[0]
         elif fp_seq:
             cur.execute(
                 "select target_gene.chromosome, target_gene.genome_build, target_gene.direction_of_transcription from dna_oligo "
