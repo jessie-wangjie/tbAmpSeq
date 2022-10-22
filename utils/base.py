b74727dd90861efb6afe1aa59e0093496dcaa7ed
jwang <jwang@tome.bio>
2022-10-21 20:54:30 -0400

    add single sample parameter

--- utils/base.py
+++ utils/base.py
@@ -8,8 +8,8 @@ username = os.getenv('WAREHOUSE_USERNAME')
 password = os.getenv('WAREHOUSE_PASSWORD')
 url = os.getenv('WAREHOUSE_URL')
 
-# conn = psycopg2.connect(f"dbname=warehouse user={username} password={password} port=5432 host={url}")
-# cur = conn.cursor()
+conn = psycopg2.connect(f"dbname=warehouse user={username} password={password} port=5432 host={url}")
+cur = conn.cursor()
 
 api_key = os.getenv('API_KEY')
 api_url = os.getenv('API_URL')
@@ -23,8 +23,8 @@ test_username = os.getenv('TEST_WAREHOUSE_USERNAME')
 test_password = os.getenv('TEST_WAREHOUSE_PASSWORD')
 test_url = os.getenv('TEST_WAREHOUSE_URL')
 
-conn = psycopg2.connect(f"dbname=warehouse user={test_username} password={test_password} port=5432 host={test_url}")
-cur = conn.cursor()
+# conn = psycopg2.connect(f"dbname=warehouse user={test_username} password={test_password} port=5432 host={test_url}")
+# cur = conn.cursor()
 
 test_api_key = os.getenv('TEST_API_KEY')
 test_api_url = os.getenv('TEST_API_URL')
