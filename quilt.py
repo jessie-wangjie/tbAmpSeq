ded9c161d6ceec5f19e0a92aa8b2a999c4e6dd6b
jwang <jwang@tome.bio>
2022-11-17 15:43:24 -0500

    add quilt script

--- quilt.py
+++ quilt.py
@@ -1,5 +1,6 @@
 import argparse
 import glob
+
 import altair as alt
 import pandas as pd
 import quilt3
@@ -14,19 +15,28 @@ if __name__ == "__main__":
 
     # plate plot
     files = glob.glob(tbid + "/*/CRISPResso_stats.json")
-    stats = pd.DataFrame()
+
+    data = pd.DataFrame()
     for s in files:
-        stats = pd.concat([stats, pd.read_json(s, orient="index").T])
-    stats["x"] = stats["well"].str.get(-1)
-    stats["y"] = stats["well"].str.get(0)
+        stats = pd.concat([data, pd.read_json(s, orient="index").T])
+    data["x"] = data["well"].str.get(-1)
+    data["y"] = data["well"].str.get(0)
+
+    # plots
+    selector = alt.selection_single(empty='all', fields=['sample_name'])
+    base = alt.Chart(data).properties(width=250, height=250).add_selection(selector)
 
     # draw plate plots
-    chart = alt.Chart(stats).mark_circle().encode(
-        x='x:O',
-        y='y:O',
-        size='beacon_placement_percentage:Q'
-    )
-    chart.save(tbid + "plate.json")
+    heatmap = base.mark_circle().encode(x='x:O', y='y:O', size='beacon_placement_percentage:Q',
+                                        alt.condition(selector, 'sample_name:O', alt.value('lightgray'), legend=None))
+    base.save(tbid + "plate.json")
+
+    # draw bar plots
+    bar = base.transform_fold(["beacon_placement_percentage", "perfect_beacon_percent"]).mark_bar().encode(
+        x='sample_name:O', y='value:Q', color='key:N')
+
+    chart = heatmap | bar
+    chart.save(tbid + "report.json")
 
     # Create test directories
     TEST_DIR = "test_workflow"
@@ -46,7 +56,7 @@ if __name__ == "__main__":
     p = quilt3.Package.browse('jwang/test_data')
 
     # adding data
-    p.set(tbid + "plate.json")
+    p.set(tbid + "report.json")
 
     # Saving a package manifest locally
     top_hash = p.build("jwang/test_data")
