49d915453bbaa5bf57c47e483c3ceffbe944d847
jwang <jwang@tome.bio>
2022-11-18 00:25:15 -0500

    add quilt script

--- quilt.py
+++ quilt.py
@@ -32,7 +32,7 @@ if __name__ == "__main__":
 
     # draw bar plots
     bar = base.transform_fold(["beacon_placement_percentage", "perfect_beacon_percent"]).mark_bar().encode(
-        x='sample_name:O', y='value:Q', color='key:N')
+        x='sample_name:O', y='value:Q', color='key:N', column='sample_name:O')
         # color=alt.condition(selector, alt.ColorValue("steelblue"), alt.ColorValue("grey"))).add_selection(selector)
 
     chart = heatmap | bar
