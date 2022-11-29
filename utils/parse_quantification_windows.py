8ac00764eb4a10b2172a222ff57e6e28dc8722bd
jwang <jwang@tome.bio>
2022-11-29 15:20:40 -0500

    add 3P script

--- utils/parse_quantification_windows.py
+++ utils/parse_quantification_windows.py
@@ -66,7 +66,10 @@ def main():
 
         df = df_ref.apply(lambda row: get_modified_in_quantification_window(row, set(range(int(start) - 1, int(end)))),
                           axis=1, result_type='expand')
+        print(df)
         g = df.groupby("classification").sum()
+        print("\n\n")
+        print(g)
         for i in g.index:
             stats[i] = g.loc[i]["#Reads"]
             if i == "modified":
