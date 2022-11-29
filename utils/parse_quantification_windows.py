bef21f1adb10e404451242fa513f6164af5bc7b2
jwang <jwang@tome.bio>
2022-11-29 16:28:55 -0500

    add 3P script

--- utils/parse_quantification_windows.py
+++ utils/parse_quantification_windows.py
@@ -73,7 +73,7 @@ def main():
         print(g)
         gg = df.groupby(["classification","n_indel"]).sum()
         print(gg.index.names)
-        print(gg.loc["modified","#Reads"])
+        g["modified", "n_indel"] = gg.loc["modified", "#Reads"]
         for i in g.index:
             stats[i] = g.loc[i]["#Reads"]
             if i == "modified":
