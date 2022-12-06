9fb56da4f9ecf1d602bcb6fcc8b150482824d3d6
jwang <jwang@tome.bio>
2022-12-06 14:11:44 -0500

    add 3P script

---
+++ write_to_benchling.test.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+"""
+On-Target analysis for Amp-seq and 3Primer-Seq
+Information from Benchling
+"""
+
+import argparse
+import glob
+import pandas as pd
+from benchling_api_client.models.naming_strategy import NamingStrategy
+from benchling_sdk.auth.api_key_auth import ApiKeyAuth
+from benchling_sdk.benchling import Benchling
+from benchling_sdk.helpers.serialization_helpers import fields
+from benchling_sdk.models import CustomEntityCreate, AssayResultCreate, AssayFieldsCreate
+
+from utils.base_test import *
+from utils.common_functions_test import *
+
+
+def main():
+    # Parse command line options
+    parser = argparse.ArgumentParser(description='Write JSON to Benchling',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("-m", help='TB id')
+
+    args = parser.parse_args()
+    tbid = args.m
+
+    # create pipeline run entity
+    # TODO: to check run suffix
+    benchling = Benchling(url=api_url, auth_method=ApiKeyAuth(api_key))
+    entity = CustomEntityCreate(schema_id=schema_id, folder_id=folder_id, registry_id=registry_id,
+                                naming_strategy=NamingStrategy.NEW_IDS,
+                                name=tbid + "a",
+                                fields=fields(
+                                    {"Genomics AmpSeq Project Queue": {"value": tbid},
+                                     "pipeline Name": {"value": "tbAmpseq"}}))
+#    pipeline_run_entity = benchling.custom_entities.create(entity)
+
+    # insert the cs2 stats to benchling
+    files = glob.glob(tbid + "/*/CRISPResso_stats.json")
+    data = pd.DataFrame()
+    for s in files:
+        data = pd.concat([data, pd.read_json(s, orient="index").T])
+
+    row = AssayResultCreate(schema_id=result_schema_id, fields=AssayFieldsCreate.from_dict(data.to_json()), project_id=result_project_id)
+    benchling.assay_results.create([row])
+
+if __name__ == "__main__":
+    main()
