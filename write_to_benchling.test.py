d78807a28cbec51af1a528ed55e931ef7a456ffb
jwang <jwang@tome.bio>
2022-12-06 14:51:57 -0500

    add script for writing to benchling

--- write_to_benchling.test.py
+++ write_to_benchling.test.py
@@ -39,10 +39,10 @@ def main():
 
     # insert the cs2 stats to benchling
     files = glob.glob(tbid + "/*/CRISPResso_stats.json")
-    data = pd.DataFrame()
+    data = {}
     for s in files:
-        data = json.load(open(s))
-        print(data)
+        data = data.append(json.load(open(s)))
+    print(data)
 #    row = AssayResultCreate(schema_id=result_schema_id, fields=AssayFieldsCreate.from_dict(data), project_id=result_project_id)
 #    benchling.assay_results.create([row])
 
