d252cbcb5d03c279937dfc8ef64f72b4cd6bd988
jwang <jwang@tome.bio>
2022-12-11 11:36:52 -0500

    add script for writing to benchling

--- bs_tracker.py
+++ bs_tracker.py
@@ -25,28 +25,32 @@ def send_email(run_id, samples):
 
 
 if __name__ == '__main__':
-    current_run = {}
+    current_run = []
+    current_run = {'TB_MISEQ_000084': 248990782, 'TB_MISEQ_000085': 249054818}
     while True:
         response = requests.get(
             f'{bs_api_server}/runs?access_token={bs_access_token}&sortby=DateCreated&SortDir=Desc&limit=5', stream=True)
         for run in response.json().get("Items"):
             samples = {}
             if run["Status"] != "Complete":
-                current_run[run["ExperimentName"]] = run["Href"]
+                current_run[run["ExperimentName"]] = run["V1Pre3Id"]
                 print(current_run)
             elif run["ExperimentName"] in current_run:
+                print(current_run[run["ExperimentName"]])
                 response = requests.get(
-                    f'{current_run[run["ExperimentName"]]}/properties/Input.BioSamples/items?access_token={bs_access_token}&limit=1000',
+                    f'{bs_api_server}/datasets?InputRuns={current_run[run["ExperimentName"]]}&access_token={bs_access_token}&limit=1000',
                     stream=True)
                 for item in response.json().get("Items"):
-                    project = item.get("BioSample").get("DefaultProject").get("Name")
-                    samples[project] = item.get("BioSample").get("DefaultProject").get("Id")
+                    project = item.get("Project").get("Name")
+                    if project != "Unindexed Reads":
+                        samples[project] = item.get("Project").get("Id")
                 send_email(run["ExperimentName"], samples.keys())
                 del current_run[run["ExperimentName"]]
 
                 # download fastq files from basespace
+                print(samples.items())
                 for s, id in samples.items():
                     subprocess.call("bs download project -i %s -o %s --extension=fastq.gz" % (id, s), shell=True)
                     subprocess.call("python /home/ubuntu/bin/tbOnT/tbAmpSeq.B2B.py -m %s -i %s -p 8 -o %s" % (s, s, s + "_tbAmpSeq"), shell=True)
 
-        time.sleep(7200)
+        time.sleep(100)
