c6ceb277a30d480c2a9d3831f86c750f116a5b39
jwang <jwang@tome.bio>
2022-12-15 13:29:51 -0500

    add script for writing to benchling

--- quilt.py
+++ quilt.py
@@ -42,30 +42,17 @@ if __name__ == "__main__":
     data.to_csv(tbid + "/stats.csv", index=False)
 
     # plots
-    selector = alt.selection_single(empty='all', fields=['sample_name'])
-    base = alt.Chart(data)
-
     # draw plate plots
-    heatmap = base.mark_circle().properties(width=300, height=200).encode(x=alt.X('x:O'), y='y:O',
+    heatmap = alt.Chart(data).mark_circle().properties(width=300, height=200).encode(x=alt.X('x:O'), y='y:O',
                 size='beacon_placement_percentage:Q', color=alt.ColorValue("#1f77b4"))
 
-    heatmap2 = heatmap.mark_circle().encode(size='perfect_beacon_percent:N', color=alt.ColorValue("#ff7f01"))
+    heatmap2 = heatmap.mark_circle().encode(size='perfect_beacon_percent:Q', color=alt.ColorValue("#ff7f01"))
 
     chart = alt.hconcat(heatmap+heatmap2)
     chart.save(tbid + "/report.json")
 
-    # draw bar plots
-#    bar = base.transform_fold(["beacon_placement_percentage", "perfect_beacon_percent"],
-#                              as_=['beacon', 'percent']).mark_bar(opacity=0.7).properties(width=600, height=200).encode(
-#        x='sample_name:O', y=alt.Y('percent:Q', stack=None),
-#        color=alt.condition(selector, alt.Color("beacon:N"), alt.ColorValue("grey"))).add_selection(selector)
-
-#    chart = alt.hconcat(heatmap, bar).resolve_scale(color="independent")
-#    chart.save(tbid + "/report.json")
-
     # create test data
-#    p = quilt3.Package()
-#    p.set("data.csv", "s3://tb-ngs-quilt/CRISPResso_on_9/CRISPResso_quantification_of_editing_frequency.txt", meta={"type": "csv"})
+    p = quilt3.Package()
 
     # edit a preexisting package
 #    quilt3.Package.install(
@@ -75,19 +62,19 @@ if __name__ == "__main__":
 #    p = quilt3.Package.browse("jwang/" + tbid)
 
     # adding data
-#    p.set("stats.csv", tbid + "/stats.csv")
-#    p.set("report.json", tbid + "/report.json")
-#    p.set_dir("alignment_html", tbid + "/alignment_html/")
-#    preview = pd.Series(["report.json", "stats.csv"])
-#    preview.to_json(tbid + "/quilt_summarize.json", orient="records")
-#    p.set("quilt_summarize.json", tbid + "/quilt_summarize.json")
+    p.set("stats.csv", tbid + "/stats.csv")
+    p.set("report.json", tbid + "/report.json")
+    p.set_dir("alignment_html", tbid + "/alignment_html/")
+    preview = pd.Series(["report.json", "stats.csv"])
+    preview.to_json(tbid + "/quilt_summarize.json", orient="records")
+    p.set("quilt_summarize.json", tbid + "/quilt_summarize.json")
 
     # Pushing a package to a remote registry
-#    with Capturing() as output:
-#        p.push(
-#            "jwang/" + tbid,
-#            "s3://tb-ngs-quilt",
-#        )
-#    base_url = output[1].split()[-1]
-#    full_url = f"{base_url}/tree/{p.top_hash}"
-#    print(full_url)
+    with Capturing() as output:
+        p.push(
+            "jwang/" + tbid,
+            "s3://tb-ngs-quilt",
+        )
+    base_url = output[1].split()[-1]
+    full_url = f"{base_url}/tree/{p.top_hash}"
+    print(full_url)
