78838810746cd6301758ce13b972bbb624df5f7c
jwang <jwang@tome.bio>
2023-01-27 19:37:58 -0500

    fix psycopg2.connect

--- utils/base.py
+++ utils/base.py
@@ -8,7 +8,7 @@ username = os.getenv('WAREHOUSE_USERNAME')
 password = os.getenv('WAREHOUSE_PASSWORD')
 url = os.getenv('WAREHOUSE_URL')
 
-conn = psycopg2.connect(dbname="warehouse", user=username, password=password, port=5432, sslmode='verify-ca', host=url)
+conn = psycopg2.connect(dbname="warehouse", user=username, password=password, port=5432, sslmode='verify-ca', host=url, async_=True)
 
 api_key = os.getenv('API_KEY')
 api_url = os.getenv('API_URL')
