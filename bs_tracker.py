1a5fd1944ead481da2edc846bf7a75b879edcf4c
jwang <jwang@tome.bio>
2023-02-03 00:44:06 -0500

    add trim adapters

--- bs_tracker.py
+++ bs_tracker.py
@@ -26,7 +26,7 @@ def send_email(run_id, samples):
 
 
 if __name__ == '__main__':
-    current_run = {'TB_MISEQ_000102': '252171937'}
+    current_run = {}
     while True:
         response = requests.get(
             f'{bs_api_server}/runs?access_token={bs_access_token}&sortby=DateCreated&SortDir=Desc&limit=5', stream=True)
