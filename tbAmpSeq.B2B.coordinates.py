ec9f3d38a8f39e667b50f973c90e6f4c91ef5ce7
jwang <jwang@tome.bio>
2023-02-06 14:41:26 -0500

    testing the bechling insertion

--- tbAmpSeq.B2B.coordinates.py
+++ tbAmpSeq.B2B.coordinates.py
@@ -316,8 +316,7 @@ def main():
 
             cs2_stats.update(window_quantification(os.path.join(output, "CRISPResso_on_" + name), [wt_qw1]))
 
-        print(cs2_stats)
-        print(ngs_stats)
+        print(pd.Series(cs2_stats).append(ngs_stats))
         pd.Series(cs2_stats).append(ngs_stats).to_json(os.path.join(output, "CRISPResso_on_" + name, "CRISPResso_stats.json"))
 
         # plot
